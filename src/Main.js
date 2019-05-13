83fab596ca9b60677244d822a970aaf1d8763615
flege <abd.alfaruq@gmail.com>
2019-05-13 14:00:11 +0700

    first init

---
+++ src/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1 className="title">Fetching Data API</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+ 
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+          </div>
+        </div>
+        </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
