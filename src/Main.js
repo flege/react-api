65bffe34d05bccfdf4dd2e3d938ed44768107275
flege <abd.alfaruq@gmail.com>
2019-05-16 08:19:33 +0700

    add gender menu

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import {Route, NavLink, HashRouter } from "react-router-dom";
 import Home from "./Home";
+import Male from "./Male";
+import Female from "./Female";
  
 class Main extends Component {
   render() {
@@ -10,10 +12,14 @@ class Main extends Component {
           <h1 className="title">Fetching Data API</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink exact to="/male">Male</NavLink></li>
+            <li><NavLink exact to="/female">Female</NavLink></li>
  
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
+            <Route exact path="/male" component={Male}/>
+            <Route exact path="/female" component={Female}/>
           </div>
         </div>
         </HashRouter>
